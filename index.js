452b3d84bd4a1088d77c97b805db000e4160296a
ayushmaan <ayushmaanroy.12@gmail.com>
2023-07-09 12:28:51 +0530

    setup the express router

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require("express");
 
 const port = 8000;
 
+const db = require("./config/mongoose");
+
 const app = express();
+
+// use express router
+app.use("/", require("./routes"));
 
 app.listen(port, function (err) {
   if (err) {
