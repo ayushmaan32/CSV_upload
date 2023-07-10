2e3e877af3b4fbc39a0dc5e26346ed2b6ea73186
ayushmaan <ayushmaanroy.12@gmail.com>
2023-07-10 17:28:53 +0530

    upload list rendered in home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,14 @@
-module.exports.home = function (req, res) {
-  return res.render("home", {
-    title: "CSV Reader",
-  });
+const File = require("../models/csv");
+// controller for home page
+module.exports.home = async function (req, res) {
+  try {
+    let file = await File.find({});
+    return res.render("home", {
+      csvfiles: file,
+      title: "CSV Reader",
+    });
+  } catch (error) {
+    console.log("Error in loading page", error);
+    return;
+  }
 };
