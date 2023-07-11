c218ab9f3cd00be93a6c8629d8f2c1770496ba89
ayushmaan <ayushmaanroy.12@gmail.com>
2023-07-11 20:20:22 +0530

    added js file for search

---
+++ assets/js/viewfile.js
@@ -0,0 +1,16 @@
+console.log("loaded");
+const searchInput = document.getElementById("search-input");
+const searchButton = document.getElementById("search-btn");
+const tableRows = document.querySelectorAll(".csv-table tbody tr");
+
+searchButton.addEventListener("click", () => {
+  const searchTerm = searchInput.value.toLowerCase();
+  for (const row of tableRows) {
+    const rowText = row.textContent.toLowerCase();
+    if (rowText.includes(searchTerm)) {
+      row.style.display = "table-row";
+    } else {
+      row.style.display = "none";
+    }
+  }
+});
